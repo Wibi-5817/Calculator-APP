787ef3967b7fb88cdd81442ebcb29323d66a3b45
Treat Pk <wtreatmaja78@students.amikom.ac.id>
2024-03-05 12:16:20 +0700

    Modifikasi

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,14 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-		
+		 if (menu == 1)
+            {
+                Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + penambahan(a, b));
+            }
+            else(menu == 2)
+            {
+                Console.WriteLine("Hasil Pengurangan {0} - {1} = {2} ", a, b, pengurangan(a, b));
+            }
 	}
 	static int Penambahan(int a, int b)
 	{
