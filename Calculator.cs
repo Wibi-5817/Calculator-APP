f51fdb081313e3baebb880c40fc731879c420620
Treat Pk <wtreatmaja78@students.amikom.ac.id>
2024-03-05 12:09:40 +0700

    pengurangan fungsi perjumlahan

--- Calculator.cs
+++ Calculator.cs
@@ -8,4 +8,12 @@ class Calculator
 	{
 		return a + b;
 	}
+	 static int penambahan(int a, int b)
+    {
+            return a + b;
+    }
+        static int pengurangan(int a, int b)
+    {
+            return a - b;
+    }
 }
