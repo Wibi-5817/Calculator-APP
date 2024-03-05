254fe69e628043e8f1d5ab5ccaa86a31711aa1a8
Treat Pk <wtreatmaja78@students.amikom.ac.id>
2024-03-05 12:18:42 +0700

    Fitur pengurangan

--- Calculator.cs
+++ Calculator.cs
@@ -15,10 +15,6 @@ class Calculator
 	{
 		return a + b;
 	}
-	 static int penambahan(int a, int b)
-    {
-            return a + b;
-    }
         static int pengurangan(int a, int b)
     {
             return a - b;
